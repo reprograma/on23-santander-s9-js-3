c03f0550408745dacaf9b86e400163e56eeca850
isamoreira <babellamoreira@gmail.com>
2023-05-26 20:13:00 -0300

    Primeiro commit com a entrega pronta, html, css e javascript

---
+++ exercicios/Isabella-Moreira/projeto-casa/script.js
@@ -0,0 +1,101 @@
+const json = [
+  {
+    "imagem": "./images/Pose.jpg",
+    "titulo": "Pose",
+    "ano": "2018",
+    "diretor": "Ryan Murphy",
+    "generos": ["Drama"],
+    "elenco": ["Dominique Jackson", "Indya Moore", "Mj Rodriguez", "Angelica Ross", "Hailie Sahar"], 
+    "instagram": "https://www.instagram.com/poseonfx/"
+  },
+  {
+    "imagem": "./images/manhas.jpg",
+    "titulo": "Manhãs de Setembro",
+    "ano": "2021",
+    "diretor": "Luis Pinheiro",
+    "generos": ["Drama"],
+    "elenco": ["Liniker", "Linn da Quebrada"], 
+    "instagram": "https://www.instagram.com/explore/tags/manhasdesetembroserie/"
+  },
+  {
+    "imagem": "./images/euphoria.jpg",
+    "titulo": "Euphoria",
+    "ano": "2019",
+    "diretor": "Sam Levinson",
+    "generos": ["Drama"],
+    "elenco": ["Hunter Schafer", "Ron Leshem", "Daphna Levin"], 
+    "instagram": "https://www.instagram.com/euphoria/"
+  },
+  {
+    "imagem": "./images/veneno.jpeg",
+    "titulo": "Veneno",
+    "ano": "2020",
+    "diretor": "Javier Ambrossi",
+    "generos": ["Drama", "Biografia"],
+    "elenco": ["Daniela Santiago", "Jedet Sánchez", "Isabel Torres", "Lola Rodríguez", "Paca La Piraña"], 
+    "instagram": "https://www.instagram.com/venenolaserie/"
+  },
+  {
+    "imagem": "./images/legendary.jpg",
+    "titulo": "Legendary",
+    "ano": "2020",
+    "diretor": "Rik Reinholdtsen",
+    "generos": ["Reality Show", "Competição"],
+    "elenco": ["Leiomy Maldonado", "Megan Thee Stallion", "Dashaun Wesley"], 
+    "instagram": "https://www.instagram.com/legendarymax/"
+  }
+]
+
+function criarCards(serie){
+
+  var card = document.createElement('div');
+  card.classList.add('card');
+
+  var imagem = document.createElement('img');
+  imagem.src = serie.imagem;
+  card.appendChild(imagem);
+
+  var titulo = document.createElement('h2');
+  titulo.textContent = serie.titulo + ' (' + serie.ano + ')';
+  card.appendChild(titulo);
+
+  var diretor = document.createElement('p');
+  diretor.textContent = 'Diretor: ' + serie.diretor;
+  card.appendChild(diretor);
+
+  var generos = document.createElement('p');
+  generos.textContent = 'Gêneros: ' + serie.generos;
+  card.appendChild(generos);
+
+  var elenco = document.createElement('p');
+  elenco.textContent = 'Elenco: '+ serie.elenco;
+  card.appendChild(elenco);
+
+  var link = document.createElement('a');
+  link.href = serie.instagram;
+  link.textContent = 'Instagram';
+  card.appendChild(link);
+
+
+  return card;
+}
+
+
+var container = document.getElementById('cardsContainer');
+
+json.forEach(function(serie) {
+  var card = criarCards(serie);
+  container.appendChild(card);
+});
+
+
+
+/* 
+O exercício consiste em usar JavaScript para popular os campos corretamente com os dados do arquivo data.json (ou do objeto JSON) um site de informações sobre séries protagonizadas por mulheres trans e travestis, neste caso a página exibe 5 séries, em 5 cards diferentes em uma só página;
+
+Importante:
+
+1. Crie seu próprio layout usando HTML e CSS nos arquivos já criados nesta pasta;
+2. Repare que este Json é uma matriz, logo é possível usar métodos de array para acessar seu conteúdo;
+3. Entregue este exercício da maneira que conseguir, use o exemplo da sala para guiar nesta construção;
+*/
